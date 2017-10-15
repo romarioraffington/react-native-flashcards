7c92a7b6577acbef7c1f6dd55e2b71f2e74cf0e5
Romario Raffington <romarioraff@gmail.com>
2017-10-15 13:49:42 -0500

    fix: Made it so the ListView doesn't bounce up on scroll

--- src/components/DeckList/index.js
+++ src/components/DeckList/index.js
@@ -1,51 +1,44 @@
 // External Dependencies
 import React from 'react'
-import { FlatList  } from 'react-native'
+import { View, FlatList} from 'react-native'
+import styled from 'styled-components/native'
 
 // Our Components
 import Deck from './components/Deck'
 
 export default () => (
-  <FlatList 
-    data={data}
-    keyExtractor = {({ id }) => id }
-    renderItem={({ item }) => <Deck deck={item} />}
-  />
+  <Container>
+    <FlatList 
+      data={data}
+      keyExtractor = {({ id }) => id }
+      renderItem={({ item }) => <Deck deck={item} />}
+    />
+  </Container>
 )
 
+// Styled Components
+const Container = styled.View`
+  flex: 1
+`
 // Temporary Datastore
 const data = [{
   id: 1,
   name: 'Jamaica',
-  cardCount: 9,
+  size: 9,
 }, {
   id: 2,
   name: 'Greenland',
-  cardCount: 5,
+  size: 5,
 }, {
   id: 3,
   name: 'London',
-  cardCount: 20,
+  size: 20,
 },{
   id: 4,
   name: 'USA',
-  cardCount: 5,
+  size: 5,
 },{
   id: 6,
   name: 'Italy',
-  cardCount: 0,
-},{
-  id: 7,
-  name: 'Italy',
-  cardCount: 1,
-}
-,{
-  id: 8,
-  name: 'Italy',
-  cardCount: 11,
-}
-,{
-  id: 9,
-  name: 'Italy',
-  cardCount: 15,
+  size: 0,
 }]
