4c9b372344db19fee46e6907f08433c68c6f109e
Romario Raffington <romarioraff@gmail.com>
2017-10-15 13:17:32 -0500

    refactor: Used cards without pictures instead of swipeable component

--- src/components/DeckList/components/Deck/index.js
+++ src/components/DeckList/components/Deck/index.js
@@ -1,96 +1,47 @@
 // External Dependencies
-import React, { Component } from 'react'
+import React from 'react'
+import { View, Text } from 'react-native'
 import styled from 'styled-components/native'
-import Swipeable from 'react-native-swipeable';
-import { Ionicons, SimpleLineIcons } from '@expo/vector-icons'
-import { View, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
 
-// Our Components
-import Header from '../../../Styled/components/Header'
-import Text from '../../../Styled/components/Text'
-import ImageContainer from '../../../Styled/components/ImageContainer'
-
-// Our Dependencies
-import { getRandomInt } from '../../../../utils'
-
-// Grab all the backgrounds. 
-// Workaround for https://github.com/facebook/react-native/issues/2481
-
-const IMAGES = [
-  require('./images/yellow-background.png'),
-  require('./images/green-background.png'),
-  require('./images/orange-background.png'),
-  require('./images/blue-background.png'),
-]
 export default ({ deck }) => (
-  <Swipeable leftButtons={leftButtons} rightButtons={rightButtons}>
-    <TouchableWithoutFeedback onPress={console.log('Deck Pressed')}>
-      <DeckImageContainer source={IMAGES[getRandomInt(0, 3)]}>
-        <CardCount>
-          <Text>{deck.cardCount}</Text>
-          <SimpleLineIcons 
-            size={15} 
-            name='layers'
-            color={'white'}
-            style={{backgroundColor: 'transparent'}}  
-            />
-        </CardCount>
-        <Header>{deck.name}</Header>
-      </DeckImageContainer>
-    </TouchableWithoutFeedback>
-  </Swipeable>
+  <Deck>
+    <Country>{deck.name}</Country>
+    <Badge>
+      <BadgeText>
+        { deck.size === 0 ? 'No': deck.size } Cards</BadgeText>
+    </Badge>
+  </Deck>
 )
 
 // Styled Components
-const DeckImageContainer = styled(ImageContainer)`
-  flex: 1
-  flex-direction: row
+const Deck = styled.View`
+  height: 139
+  margin-left: 20
+  margin-right: 20
+  margin-bottom: 10
+  border-radius: 6
+  shadow-radius: 9;
+  shadow-opacity: 0.4
+  shadow-color: #D9E2E9
+  background-color: #FFF
   align-items: center
-
-`
-const SwipeableButton = styled.TouchableOpacity`
-  flex: 1
-  padding-right: 30
-  padding-left: 30
   justify-content: center
-  background-color: ${props => props.delete ? '#D0021B' : '#3F81DC'};
 `
-const SwipeableButtonLeft = styled(SwipeableButton)`
-  align-items: flex-end
+const Country = styled.Text`
+  color: #485465
+  font-size: 24
 `
-const CardCount = styled.View`
-  margin-left: 30
-  flex-direction: row
-  align-self: flex-start
+const Badge = styled.View`
+  width: 57
+  height: 22
+  border-radius: 4
+  opacity: 0.6
+  align-items: center
+  justify-content: center
+  background-color: #A8B4C4
 `
-// Swipeable Buttons
-const leftButtons = [
-  <SwipeableButtonLeft onPress={console.log('Quiz Pressed')}>
-    <Ionicons
-      size={40}
-      color={'white'}
-      name='ios-book-outline'
-    />
-    <Text>Quiz</Text>
-  </SwipeableButtonLeft>,
-];
-
-const rightButtons = [
-  <SwipeableButton onPress={console.log('Add Pressed')}>
-    <Ionicons
-      size={40}
-      color={'white'}
-      name='ios-add'
-    />
-    <Text>Card</Text>
-  </SwipeableButton>,
-  <SwipeableButton delete onPress={console.log('Delete Pressed')}>
-    <Ionicons
-      size={40}
-      color={'white'}
-      name='ios-trash-outline'
-    />
-    <Text>Deck</Text>
-  </SwipeableButton>,
-];
-
+const BadgeText = styled.Text`
+  color: #FFF
+  font-weight: 600
+  font-size: 12
+`
