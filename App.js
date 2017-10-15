674f57095981abe564112a6bbde8b628d2df6b5d
Romario Raffington <romarioraff@gmail.com>
2017-10-14 19:06:43 -0500

    feat: Made the app component load the custom fonts

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 // External Dependencies
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { Font } from 'expo'
+import styled from 'styled-components/native'
+import { Text, View } from 'react-native'
 
 // Our Dependencies
-import Deck from './src/components/Deck'
+import Home from './src/screens/Home'
 
 export default class App extends React.Component {
+  state = {
+    isFontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'avenir-roman': require('./src/assets/fonts/AvenirLTStd-Roman.otf'),
+      'avenir-heavy': require('./src/assets/fonts/Avenir-Heavy.ttf'),
+    });
+
+    this.setState({ isFontLoaded: true })
+  }
+
   render() {
     return (
-      <View>
-        <Deck/>
-      </View>
+      <View>{this.state.isFontLoaded && <Home />}</View>
     );
   }
 }
