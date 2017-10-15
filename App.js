596ee08e602b51951b9b2f67feb594b9f501eed2
Romario Raffington <romarioraff@gmail.com>
2017-10-15 13:16:48 -0500

    refactor: Removed custom font

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
 // External Dependencies
-import React from 'react'
-import { Font } from 'expo'
+import React, { Component } from 'react'
 import styled from 'styled-components/native'
-import { Text, View } from 'react-native'
 
-// Our Dependencies
+// Our Components
 import Home from './src/screens/Home'
 
-export default class App extends React.Component {
-  state = {
-    isFontLoaded: false,
-  };
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'avenir-roman': require('./src/assets/fonts/AvenirLTStd-Roman.otf'),
-      'avenir-heavy': require('./src/assets/fonts/Avenir-Heavy.ttf'),
-    });
-
-    this.setState({ isFontLoaded: true })
-  }
-
+export default class App extends Component {
   render() {
     return (
-      <View>{this.state.isFontLoaded && <Home />}</View>
-    );
+      <Container>
+        <Home />
+      </Container>
+    )
   }
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #FEFEFE;
+`
