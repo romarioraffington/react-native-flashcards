1178d6220187cd017493a98d1bbd0e60e72c29b3
Romario Raffington <romarioraff@gmail.com>
2017-10-15 13:24:20 -0500

    feat: Added StatusCard and ActionButton component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,32 @@
 // External Dependencies
-import React from 'react'
-import { View } from 'react-native'
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native'
+import styled from 'styled-components/native'
+import ActionButton from 'react-native-action-button';
 
 // Our Components
-import Header from '../../components/Header'
-import DeckList from '../../components/DeckList'
+import Header from '../../components/Header' 
+import DeckList from '../../components/DeckList' 
+import StatusCard from '../../components/StatusCard' 
 
-export default class Home extends React.Component {
+export default class Home extends Component {
   render() {
     return (
-      <View>
+      <Container>
         <Header />
+        <StatusCard />
         <DeckList />
-      </View>
+        <ActionButton 
+          buttonColor="#3B48EE" 
+          shadowStyle={{ shadowOpacity: 0.3, shadowRadius: 9 }}
+          onPress={() => { console.log("Add Button")}} 
+        />
+      </Container>
     )
   }
 }
+
+// Styled Components
+const Container = styled.View`
+  flex: 1
+`
