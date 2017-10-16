4fd06743e36384a27af5a7d2f7ca7523f67c1540
Romario Raffington <romarioraff@gmail.com>
2017-10-15 19:56:46 -0500

    feat: Made it so the user is redirected to the AddDeck screen when the ActionButton is clicked

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,21 +5,27 @@ import styled from 'styled-components/native'
 import ActionButton from 'react-native-action-button';
 
 // Our Components
+import AddDeck from '../AddDeck'
 import Header from '../../components/Header' 
 import DeckList from '../../components/DeckList' 
 import StatusCard from '../../components/StatusCard' 
 
 export default class Home extends Component {
   render() {
+   const { route, navigator } = this.props
     return (
       <Container>
-        <Header />
+        <Header title={route.title} />
         <StatusCard />
-        <DeckList />
+        <DeckList navigator={navigator} />
         <ActionButton 
           buttonColor="#3B48EE" 
           shadowStyle={{ shadowOpacity: 0.3, shadowRadius: 9 }}
-          onPress={() => { console.log("Add Button")}} 
+          onPress={() => navigator.push({ 
+            component: AddDeck , 
+            title: 'Add a Deck', 
+            backButtonIcon: require('../images/back-button.png'),
+          })}
         />
       </Container>
     )
