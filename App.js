d13d57ea5f799a1768d2dee463ea3a172b580819
Romario Raffington <romarioraff@gmail.com>
2017-10-16 00:45:57 -0500

    feat: Added redux

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 // External Dependencies
 import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import styled from 'styled-components/native'
+import devToolsEnhancer from 'remote-redux-devtools';
 
-// Our Components
+// Our Dependencies
 import Router from './src/components/Router'
+import reducer from './src/reducer' 
+
+// Boostrap store
+const store = createStore(reducer, devToolsEnhancer());
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
-        <Router />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <Router />
+        </Container>
+      </Provider>
     )
   }
 }
