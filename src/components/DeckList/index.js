deb33d0861768fb2afef69184085ccc089b1b3ca
Romario Raffington <romarioraff@gmail.com>
2017-10-18 20:57:40 -0500

    refactor: Made it so the "No Decks View" is displayed from the Home component

--- src/components/DeckList/index.js
+++ src/components/DeckList/index.js
@@ -1,8 +1,7 @@
 // External Dependencies
 import React from 'react'
 import styled from 'styled-components/native'
-import { SimpleLineIcons } from '@expo/vector-icons'
-import { View, Text, FlatList, TouchableOpacity} from 'react-native'
+import { View, FlatList } from 'react-native'
 
 // Our Components
 import Deck from './components/Deck'
@@ -19,20 +18,6 @@ export default ({ decks, navigator }) => (
         />
       </FlatListContainer>
     )}
-
-    { decks.length === 0 && (
-      <NoDecksContainer>
-        <SimpleLineIcons name='layers' size={80} color='#A8B4C4' />
-        <AddDeckButton onPress={() => {
-          navigator.push({ 
-            component: AddDeck , 
-            title: 'Add a Deck ',
-          })}
-        }>
-          <AddDeckText>Add Deck</AddDeckText>
-        </AddDeckButton>
-      </NoDecksContainer>
-    )}
   </Container>
 )
 
@@ -42,28 +27,4 @@ const Container = styled.View`
 `
 const FlatListContainer = styled.View`
   margin-top: 50
-`
-const AddDeckButton = styled.TouchableOpacity`
-  height: 53
-  width: 200
-  margin-top: 20
-  align-self: center
-  justify-content: center
-  align-items: center
-  border-radius: 4
-  shadow-radius: 3;
-  shadow-opacity: 0.1
-  shadow-color: #3B48EE
-  background-color: #3B48EE
-`
-const AddDeckText = styled.Text`
-  font-size: 17
-  color: #FFF
-  font-family: Helvetica Neue  
-`
-const NoDecksContainer = styled.View`
-  flex: 1
-  margin-top: -100
-  align-items: center
-  justify-content: center
 `
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,11 @@
 // External Dependencies
 import React, { Component } from 'react'
-import _ from 'lodash'
-import { View } from 'react-native'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import styled from 'styled-components/native'
+import { SimpleLineIcons } from '@expo/vector-icons'
 import ActionButton from 'react-native-action-button';
+import { View, TouchableOpacity, Text } from 'react-native'
 
 // Our Components
 import AddDeck from '../AddDeck'
@@ -13,34 +14,46 @@ import DeckList from '../../components/DeckList'
 import StatusCard from '../../components/StatusCard' 
 
 // Actions 
-import { getDecks } from '../../models/Deck/actions'
+import { getDecks, saveDeck } from '../../models/Deck/actions'
 
 class Home extends Component {
   componentDidMount () {
-    this.props.dispatch(getDecks())
+    this.props.getDecks()
   }
 
   render() {
-   const { decks, route, navigator } = this.props
+   const { decks, navigator, saveDeck } = this.props
     return (
       <Container>
-        <Header title={route.title} />
+        <Header />
         <StatusCard />
         <DeckList decks={decks} navigator={navigator} />
 
-        {/* Do not render Add Deck button if 
-            No decks are available as the DeckList 
-            component will render its own Add Deck button 
-        */}
         { decks.length !== 0 && (
           <ActionButton 
             buttonColor="#3B48EE" 
             shadowStyle={{ shadowOpacity: 0.3, shadowRadius: 9 }}
             onPress={() => navigator.push({ 
-              component: AddDeck , 
+              component: AddDeck, 
               title: 'Add a Deck',
+              passProps: { saveDeck } 
             })}
           />
+        )}
+
+        { decks.length === 0 && (
+          <NoDecksContainer>
+            <SimpleLineIcons name='layers' size={80} color='#A8B4C4' />
+            <AddDeckButton onPress={() => {
+              navigator.push({ 
+                component: AddDeck , 
+                title: 'Add a Deck ',
+                passProps: { saveDeck } 
+              })}
+            }>
+              <AddDeckText>Add Deck</AddDeckText>
+            </AddDeckButton>
+          </NoDecksContainer>
         )}
       </Container>
     )
@@ -59,12 +72,43 @@ const mapStateToProps = ({ deck }) => {
   }
 }
 
+const mapDispatchToProps = (dispatch => (
+  bindActionCreators({
+    getDecks,
+    saveDeck,
+  }, dispatch)
+))
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(Home)
 
 // Styled Components
 const Container = styled.View`
   flex: 1
   background-color: #FEFEFE
+`
+const AddDeckButton = styled.TouchableOpacity`
+  height: 53
+  width: 200
+  margin-top: 20
+  align-self: center
+  justify-content: center
+  align-items: center
+  border-radius: 4
+  shadow-radius: 3;
+  shadow-opacity: 0.1
+  shadow-color: #3B48EE
+  background-color: #3B48EE
+`
+const AddDeckText = styled.Text`
+  font-size: 17
+  color: #FFF
+  font-family: Helvetica Neue  
+`
+const NoDecksContainer = styled.View`
+  margin-bottom: 300
+  align-items: center
+  justify-content: center
 `
