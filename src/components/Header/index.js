169b0ddace7a47b6f292e509e39ca670672bb939
Romario Raffington <romarioraff@gmail.com>
2017-10-18 20:59:10 -0500

    refactor: Removed Header text as it was not being used

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,9 @@ import React from 'react'
 import styled from 'styled-components/native'
 import { View, Text, StatusBar } from 'react-native'
 
-export default ({ title }) => (
+export default () => (
   <Container>
    <StatusBar barStyle="light-content"/>
-    <Header>{title}</Header>
   </Container>
 )
 
@@ -17,11 +16,4 @@ const Container = styled.View`
   align-items: center
   justify-content: center
   background-color: #3B48EE
-`
-const Header = styled.Text`
-  font-size: 18
-  font-family: AvenirNext-Medium
-  margin-top: 20
-  color: #FFFFFF
-  font-weight: 500
 `
